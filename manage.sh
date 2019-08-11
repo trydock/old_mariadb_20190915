29186b64db96b987fa63b2fe2e60eff85fdd2e41
debuggerboy <debuggerboy@gmail.com>
2019-08-11 15:11:08 +0000

    update script

--- manage.sh
+++ manage.sh
@@ -63,29 +63,11 @@ if [ "$MYSQL_DATABASE" != "" ]; then
 	/usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < $tfile
 	rm -f $tfile
 
-	for f in /docker-entrypoint-initdb.d/*; do
-		case "$f" in
-			*.sql)    echo "$0: running $f"; /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*.sql.gz) echo "$0: running $f"; gunzip -c "$f" | /usr/bin/mysqld --user=mysql --bootstrap --verbose=0 --skip-name-resolve --skip-networking=0 < "$f"; echo ;;
-			*)        echo "$0: ignoring or entrypoint initdb empty $f" ;;
-		esac
-		echo
-	done
-
 	echo
 	echo 'MySQL init process done. Ready for start up.'
 	echo
 
 	echo "exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0" "$@"
 fi
-
-# execute any pre-exec scripts
-for i in /scripts/pre-exec.d/*sh
-do
-	if [ -e "${i}" ]; then
-		echo "[i] pre-exec.d - processing $i"
-		. ${i}
-	fi
-done
 
 exec /usr/bin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
